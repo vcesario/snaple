1597e3d1291ffcd4bfdd7cf738a14b4d2741f651
victor cesario <victor.cesario@hotmail.com>
2023-05-07 22:57:50 -0300

    grabbing card properties and saving to disk

--- card-scraper.py
+++ card-scraper.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import jsonpickle
+
+# declaring class
+class GameCard:
+    Id = ''
+    Energy = 0
+    Power = 0
+    Url = ''
+    PowerType = ''
 
 # reading key.conf
 keys = {}
@@ -13,10 +22,36 @@ for line in content:
 
 # opening website and fetching raw data
 driver = webdriver.Chrome(keys['chromedriver-path'])
-driver.get('https://marvelsnapzone.com/cards/')
-cards = driver.find_elements(By.CSS_SELECTOR, 'a.simple-card.hasvariants')
-cards_list = []
-for p in range(len(cards)):
-    cards_list.append(cards[p].text)
-print(cards_list)
+driver.set_page_load_timeout(5)
+
+try:
+    driver.get('https://marvelsnapzone.com/cards/')
+except:
+    pass
+
+cardListDiv = driver.find_element(By.CSS_SELECTOR, 'div.cards-list')
+cardElements = cardListDiv.find_elements(By.CSS_SELECTOR, 'a')
+
+released = []
+unreleased = []
+for e in cardElements:
+    card = GameCard()
+    card.Id = e.get_attribute('data-name')
+    card.Energy = e.get_attribute('data-cost')
+    card.Power = e.get_attribute('data-power')
+    card.Url = e.get_attribute('data-src')
+    card.PowerType = e.get_attribute('data-tags')
+    
+    cardStatus = e.get_attribute('data-status')
+
+    if cardStatus == 'released':
+        released.append(card)
+    else:
+        unreleased.append(card)
+
 driver.close()
+
+# writing data to disk
+cardStrings = jsonpickle.encode(released)
+with open('card-data.json', 'w') as cd:
+    cd.write(cardStrings)
